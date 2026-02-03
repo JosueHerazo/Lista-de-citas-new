13db14719e6c1c04d33761fb94f063c688ad72ad
Josue herazo <josuedh18@hotmail.es>
2026-02-03 01:55:42 +0100

    fix scheduled

--- src/handlers/date.ts
+++ src/handlers/date.ts
@@ -24,23 +24,23 @@ export const createProduct = async (req: Request, res: Response) => {
 }
 
 export const getBarberAvailability = async (req: Request, res: Response) => {
+    const { barber } = req.params;  
+    // Buscamos todas las citas de ese barbero
+    if (!barber) {
+     return res.status(400).json({ error: "Debe especificar un barbero" });
+    }
     try {
         // el handeler es el que hace la magia con el param de barber y busca todas las citas donde el barber y toma la lista de citas
-        const { barber } = req.params;  
-        // Buscamos todas las citas de ese barbero
         const appointment = await Datelist.findAll({
             where: { barber },
             attributes: ['dateList'] // Solo nos interesa la fecha
         });   
         // Respondemos con el array de fechas
         res.json({ data: appointment })
-           if (!barber) {
-            return res.status(400).json({ error: "Debe especificar un barbero" });
-        }
     } catch (error) {
         res.status(500).json({ error: "Error en el servidor" });
     }
-}; 
+}
 export const createAppointment= async (req: Request, res: Response) => {
     try {
         const appointment = await Datelist.create(req.body);
