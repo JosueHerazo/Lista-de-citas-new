604dcd900c2767abee269a2aaaeb5bec98efd12a
unknown <josueherazo25@gmail.com>
2026-03-22 17:55:11 +0100

    fix: remove .env from repo

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,13 @@
-import { Sequelize } from "sequelize-typescript"
+import {Sequelize} from "sequelize-typescript"
+import dotenv from "dotenv"
+
+
+
+dotenv.config()
+ 
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
+    // EL ERROR ESTABA AQUÍ: Los corchetes después de la coma estaban mal puestos
     models: [__dirname + "/../models/**/*.{ts,js}"], 
     logging: false,
     dialect: 'postgres', 
@@ -10,6 +17,9 @@ const db = new Sequelize(process.env.DATABASE_URL!, {
             rejectUnauthorized: false 
         }
     }
-})
+});
+console.log("Cloudinary Key:", process.env.CLOUDINARY_API_KEY);
+
+
 
 export default db
