d783b4655c1953fc9cf7786515124da03adb241a
unknown <josueherazo25@gmail.com>
2026-03-24 01:41:20 +0100

    Ignora carpeta dist

--- dist/models/Comment.model.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from "sequelize-typescript";
-import News from "./New.model";
-export declare class Comment extends Model {
-    text: string;
-    userName: string;
-    newsId: number;
-    news: News;
-}
-export default Comment;
