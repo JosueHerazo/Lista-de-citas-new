b6d9ba0bb470ade40452c08192d4c22f2f111006
unknown <josueherazo25@gmail.com>
2026-03-24 04:15:33 +0100

    Fix imports DateList to List

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,20 +1,13 @@
-import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import multer from 'multer';
+import multer from "multer"
+import { v2 as cloudinary } from "cloudinary"
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!
+})
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'latinos_vip_noticias',
-    resource_type: 'auto',
-    // @ts-ignore --- Esto silencia el error de TypeScript si persiste
-  } as any, 
-});
+const storage = multer.memoryStorage()
 
-export const upload = multer({ storage });
+export const uploadWork = multer({ storage })
+export { cloudinary }
