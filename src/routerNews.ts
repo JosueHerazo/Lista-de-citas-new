d65badec3d14efd412576b6c5d7336d283221091
unknown <josueherazo25@gmail.com>
2026-08-03 15:50:57 +0200

    add new link

--- src/routerNews.ts
+++ src/routerNews.ts
@@ -1,11 +1,13 @@
+
 import { Router } from 'express';
+import { uploadWork } from './config/cloudinary'; 
 // Intenta importar sin la extensión .js si tu configuración de TS lo permite, 
 // o verifica que el archivo realmente esté en src/handlers/news.ts
 import { addComment, createNews, getNews, toggleLike } from './handlers/news';
 
 const router = Router();
 
-router.post('/', createNews);
+router.post('/', uploadWork.single('file'), createNews);
 router.get('/', getNews); 
 router.post('/:newsId/comments', addComment);
 router.post('/:newsId/like', toggleLike);  
